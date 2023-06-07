a30f381178552c0b41adf33c17a74b0940408371
Leen-Rasllan <leenrasllan@gmail.com>
2023-06-07 23:33:30 +0000

    product page and cart

--- main.js
+++ main.js
@@ -79,3 +79,273 @@ function countDown(){
 setInterval(function(){
     countDown();
 },1000)
+
+
+
+let list = document.querySelectorAll('.list .item');
+list.forEach(item => {
+    item.addEventListener('click', function(event){
+        if(event.target.classList.contains('add')){
+            var itemNew = item.cloneNode(true);
+            let checkIsset  = false;
+
+            let listCart = document.querySelectorAll('.cart .item');
+            listCart.forEach(cart =>{
+                if(cart.getAttribute('data-key') == itemNew.getAttribute('data-key')){
+                    checkIsset = true;
+                    cart.classList.add('danger');
+                    setTimeout(function(){
+                        cart.classList.remove('danger');
+                    },1000)
+                }
+            })
+            if(checkIsset == false){
+                document.querySelector('.listCart').appendChild(itemNew);
+            }
+
+        }
+    })
+})
+function Remove($key){
+    let listCart = document.querySelectorAll('.cart .item');
+    listCart.forEach(item => {
+        if(item.getAttribute('data-key') == $key){
+            item.remove();
+            return;
+        }
+    })
+}
+
+const addToCartButtons = document.querySelectorAll('.box .btn');
+
+// Create an empty cart object to store the selected products
+const cart = {};
+
+// Add event listeners to the add to cart buttons
+addToCartButtons.forEach((button) => {
+  button.addEventListener('click', addToCart);
+});
+
+// Event handler for the add to cart button
+function addToCart(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the parent box element
+  const box = event.target.closest('.box');
+
+  // Get the product details from the box element
+  const productId = box.getAttribute('data-product-id');
+  const productName = box.querySelector('h3').textContent;
+  const productPrice = parseFloat(box.querySelector('.price').textContent.replace('$', ''));
+  const productQuantity = parseInt(box.querySelector('.quantity input').value);
+
+  // Add the product to the cart object
+  cart[productId] = {
+    name: productName,
+    price: productPrice,
+    quantity: productQuantity
+  };
+
+  // Update the cart UI
+  updateCartUI();
+}
+
+// Function to update the cart UI
+function updateCartUI() {
+  // Get the cart element
+  const cartElement = document.getElementById('cart');
+
+  // Clear the cart content
+  cartElement.innerHTML = '';
+
+  // Calculate the total price
+  let totalPrice = 0;
+
+  // Loop through the selected products in the cart object
+  for (const productId in cart) {
+    if (cart.hasOwnProperty(productId)) {
+      const product = cart[productId];
+
+      // Create a new cart item element
+      const cartItem = document.createElement('div');
+      cartItem.classList.add('cart-item');
+
+      // Set the content of the cart item
+      cartItem.innerHTML = `
+        <span>${product.name}</span>
+        <span>${product.quantity}</span>
+        <span>$${product.price.toFixed(2)}</span>
+      `;
+
+      // Add the cart item to the cart element
+      cartElement.appendChild(cartItem);
+
+      // Calculate the subtotal for the current product
+      const subtotal = product.price * product.quantity;
+      totalPrice += subtotal;
+    }
+  }
+
+  // Update the total price in the cart UI
+  const totalPriceElement = document.createElement('div');
+  totalPriceElement.classList.add('total-price');
+  totalPriceElement.textContent = `Total: $${totalPrice.toFixed(2)}`;
+  cartElement.appendChild(totalPriceElement);
+}
+
+
+
+
+class CartItem{
+    constructor(name, desc, img, price){
+        this.name = name
+        this.desc = desc
+        this.img=img
+        this.price = price
+        this.quantity = 1
+   }
+}
+
+class LocalCart{
+    static key = "cartItems"
+
+    static getLocalCartItems(){
+        let cartMap = new Map()
+     const cart = localStorage.getItem(LocalCart.key)   
+     if(cart===null || cart.length===0)  return cartMap
+        return new Map(Object.entries(JSON.parse(cart)))
+    }
+
+    static addItemToLocalCart(id, item){
+        let cart = LocalCart.getLocalCartItems()
+        if(cart.has(id)){
+            let mapItem = cart.get(id)
+            mapItem.quantity +=1
+            cart.set(id, mapItem)
+        }
+        else
+        cart.set(id, item)
+       localStorage.setItem(LocalCart.key,  JSON.stringify(Object.fromEntries(cart)))
+       updateCartUI()
+        
+    }
+
+    static removeItemFromCart(id){
+    let cart = LocalCart.getLocalCartItems()
+    if(cart.has(id)){
+        let mapItem = cart.get(id)
+        if(mapItem.quantity>1)
+       {
+        mapItem.quantity -=1
+        cart.set(id, mapItem)
+       }
+       else
+       cart.delete(id)
+    } 
+    if (cart.length===0)
+    localStorage.clear()
+    else
+    localStorage.setItem(LocalCart.key,  JSON.stringify(Object.fromEntries(cart)))
+       updateCartUI()
+    }
+}
+
+
+const cartIcon = document.querySelector('.fa-cart-arrow-down')
+const wholeCartWindow = document.querySelector('.whole-cart-window')
+wholeCartWindow.inWindow = 0
+const addToCartBtns = document.querySelectorAll('.add-to-cart-btn')
+addToCartBtns.forEach( (btn)=>{
+    btn.addEventListener('click', addItemFunction)
+}  )
+
+function addItemFunction(e){
+    const id = e.target.parentElement.parentElement.parentElement.getAttribute("data-id")
+    const img = e.target.parentElement.parentElement.previousElementSibling.src
+    const name = e.target.parentElement.previousElementSibling.textContent
+    const desc = e.target.parentElement.children[0].textContent
+    let price = e.target.parentElement.children[1].textContent
+    price = price.replace("Price: $", '')
+    const item = new CartItem(name, desc, img, price)
+    LocalCart.addItemToLocalCart(id, item)
+ console.log(price)
+}
+
+
+cartIcon.addEventListener('mouseover', ()=>{
+if(wholeCartWindow.classList.contains('hide'))
+wholeCartWindow.classList.remove('hide')
+})
+
+cartIcon.addEventListener('mouseleave', ()=>{
+    // if(wholeCartWindow.classList.contains('hide'))
+    setTimeout( () =>{
+        if(wholeCartWindow.inWindow===0){
+            wholeCartWindow.classList.add('hide')
+        }
+    } ,500 )
+    
+    })
+
+ wholeCartWindow.addEventListener('mouseover', ()=>{
+     wholeCartWindow.inWindow=1
+ })  
+ 
+ wholeCartWindow.addEventListener('mouseleave', ()=>{
+    wholeCartWindow.inWindow=0
+    wholeCartWindow.classList.add('hide')
+})  
+ 
+
+function updateCartUI(){
+    const cartWrapper = document.querySelector('.cart-wrapper')
+    cartWrapper.innerHTML=""
+    const items = LocalCart.getLocalCartItems()
+    if(items === null) return
+    let count = 0
+    let total = 0
+    for(const [key, value] of items.entries()){
+        const cartItem = document.createElement('div')
+        cartItem.classList.add('cart-item')
+        let price = value.price*value.quantity
+        price = Math.round(price*100)/100
+        count+=1
+        total += price
+        total = Math.round(total*100)/100
+        cartItem.innerHTML =
+        `
+        <img src="${value.img}"> 
+                       <div class="details">
+                           <h3>${value.name}</h3>
+                           <p>${value.desc}
+                            <span class="quantity">Quantity: ${value.quantity}</span>
+                               <span class="price">Price: $ ${price}</span>
+                           </p>
+                       </div>
+                       <div class="cancel"><i class="fas fa-window-close"></i></div>
+        `
+       cartItem.lastElementChild.addEventListener('click', ()=>{
+           LocalCart.removeItemFromCart(key)
+       })
+        cartWrapper.append(cartItem)
+    }
+
+    if(count > 0){
+        cartIcon.classList.add('non-empty')
+        let root = document.querySelector(':root')
+        root.style.setProperty('--after-content', `"${count}"`)
+        const subtotal = document.querySelector('.subtotal')
+        subtotal.innerHTML = `SubTotal: $${total}`
+    }
+    else
+    cartIcon.classList.remove('non-empty')
+}
+document.addEventListener('DOMContentLoaded', ()=>{updateCartUI()})
+    
+
+
+
+
+
+
