5cbfad62c217b85b3e3a28bb052ebea564e6c447
anfalqa <anfalaldraidi2001@gmail.com>
2023-06-11 21:12:00 +0000

    updated files

---
+++ js/homepage.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let allProd=[];
+
+function prodectsHomePage(name ,img ,price){
+    this.prodName=name,
+    this.prodImg=img;
+    this.prodPrice=price
+    allProd.push(this);
+}
+
+
