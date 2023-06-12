69cd0aa402808fa2bfcc32b73e1e0c17a5d1743d
anfalqa <anfalaldraidi2001@gmail.com>
2023-06-12 07:54:06 +0000

    updating files

--- js/homepage.js
+++ js/homepage.js
@@ -9,4 +9,17 @@ function prodectsHomePage(name ,img ,price){
     allProd.push(this);
 }
 
+prodectsHomePage.prototype.renderEmp  =   function(){
+    let divs=document.getElementsByClassName("box");
+    let imgpro=document.createElement("img");
+    imgpro.src=this.prodImg;
+    imgpro.alt=this.prodName;
+    divs.appendchild(imgpro);
+}
+
+
+let productHp1 =new prodectsHomePage("Microsoft Xbox Series S","./assets/source/xbox.jpg","500$");
+let productHp2 =new prodectsHomePage("SAMSUNG 16 Galaxy Book3","./assets/source/gsmarena_001.jpg","700$")
+let productHp3 =new prodectsHomePage("Canon EF 16-35mm Lens","./assets/source/camera2.jpg","350$")
+let productHp4 =new prodectsHomePage("Headsets with microphone","./assets/source/heads1.jpg","100$")
 
