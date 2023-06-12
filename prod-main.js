f2e886168145538059ee0a07a86410aa4c8009c9
mohmmadalhilo <mohmmed.hilo15@gmail.com>
2023-06-12 07:05:41 +0000

    edit the footer and header

--- prod-main.js
+++ prod-main.js
@@ -1,6 +1,10 @@
 
 
 
+
+
+
+
 const modeToggle = document.getElementById('mode-toggle');
 const body = document.body;
 
@@ -8,30 +12,6 @@ modeToggle.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
 });
 
-
-
-
-
-
-
-
-
-
-const button = document.querySelector(".addtocart");
-const done = document.querySelector(".done");
-console.log(button);
-let added = false;
-button.addEventListener('click',()=>{
-  if(added){
-    done.style.transform = "translate(-110%) skew(-40deg)";
-    added = false;
-  }
-  else{
-    done.style.transform = "translate(0px)";
-    added = true;
-  }
-    
-});
 
 
 
@@ -290,3 +270,11 @@ function updateCartUI() {
         cartIcon.classList.remove('non-empty');
     }
 }
+   
+
+
+
+
+
+var divElement = document.getElementById('cart-box'); 
+divElement.innerHTML = JSON.stringify(cartItem)
