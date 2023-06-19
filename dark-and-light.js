1489ce63ae9b5c0db40ad591bf9074a922c27018
anfalqa <anfalaldraidi2001@gmail.com>
2023-06-19 09:33:44 +0000

    updating the files

--- dark-and-light.js
+++ dark-and-light.js
@@ -1,9 +1,3 @@
-const modeToggle = document.getElementById('mode-toggle');
-const body = document.body;
-
-modeToggle.addEventListener('click', function() {
-  body.classList.toggle('dark-mode');
-});
 
 
 let menu = document.querySelector('#menu-bar');
@@ -47,3 +41,31 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
 });
 
+let countDate = new Date('june 1, 2021 00:00:00').getTime();
+
+function countDown(){
+
+    let now = new Date().getTime();
+
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown();
+},1000)
